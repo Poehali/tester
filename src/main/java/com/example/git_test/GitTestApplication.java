c1def0a77cfa9e85dfac87563f8b9be612a0d5f8
avartru <test@yandex.ru>
2021-01-04 19:54:01 +0300

    1- commit

---
+++ src/main/java/com/example/git_test/GitTestApplication.java
@@ -0,0 +1,34 @@
+package com.example.git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestApplication.class, args);
+
+        //---------------------------------------------------------
+
+        System.out.println("Hello 1 !");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //---------------------------------------------------------
+    }
+
+}
