6792051b35a1cf9f1caf1ca659a50cd3fef9121d
Poehali <31405929+Poehali@users.noreply.github.com>
2021-01-09 16:14:20 +0300

    Update GitTestApplication.java
    77777

--- src/main/java/com/example/git_test/GitTestApplication.java
+++ src/main/java/com/example/git_test/GitTestApplication.java
@@ -13,7 +13,7 @@ public class GitTestApplication {
         //---------------------------------------------------------
 
         System.out.println("Hello 1 !");
-        System.out.println("Hello 777 DEV !");
+        System.out.println("Hello 77777 DEV !");
         System.out.println("Hello 5555 DEV !");
         System.out.println("Hello 3 DEV !");
 
